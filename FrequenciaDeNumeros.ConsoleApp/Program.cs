be2a57c311e852a0579a3318f745597f9121e1aa
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-18 16:07:44 -0300

    Add project files.

---
+++ FrequenciaDeNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequenciaDeNumeros.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de números: ");
+            int quantidadeDeNumeros = Convert.ToInt32(Console.ReadLine());
+
+            int contador = 0;
+            var listaNumeros = new int[quantidadeDeNumeros];
+
+            for (int i = 0; i < quantidadeDeNumeros; i++)
+            {
+                Console.WriteLine("Digite um número: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                listaNumeros[i] = numero;
+            }
+
+            var collectionWithDistinctElements = listaNumeros.Distinct().ToArray();
+
+            for (int i = 0; i < collectionWithDistinctElements.Length; i++)
+            {
+                var numeroAtual = collectionWithDistinctElements[i];
+
+                for (int j = 0; j < quantidadeDeNumeros; j++)
+                {
+                    if (numeroAtual == listaNumeros[i])
+                    {
+                        contador++;
+                    }
+                }
+                Console.WriteLine($"O número {numeroAtual} repete {contador} vezes");
+                contador = 0;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
